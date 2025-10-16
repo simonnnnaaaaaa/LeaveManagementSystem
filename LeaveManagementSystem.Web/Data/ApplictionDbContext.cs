3491ef7638c55e9a4deedda35ebf60ea87a55991
sboicu <sboicu@ENDAUTOGTTXV7WO>
2025-10-16 10:50:28 +0300

    Add project files.

---
+++ LeaveManagementSystem.Web/Data/ApplictionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+    }
+}
---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
+
